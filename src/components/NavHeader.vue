7be4eb850215d4d357398fa25b4ca9181d2dabbe
huangzhihui <leononon412@gmail.com>
2022-10-26 17:51:48 +0800

    add NavHeader3 mixin函数复用代码

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -36,7 +36,12 @@
             <div class="children"></div>
           </div>
         </div>
-        <div class="header-search"></div>
+        <div class="header-search">
+          <div class="wrapper">
+            <input type="text" name="keyword" />
+            <a href="javascript:;"></a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -50,6 +55,7 @@ export default {
 
 <style lang="scss">
 @import "./../assets/scss/base.scss";
+@import "./../assets/scss/mixin.scss";
 .header {
   .nav-topbar {
     height: 39px;
@@ -57,9 +63,7 @@ export default {
     background-color: #333333;
     color: #b0b0b0;
     .container {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
+      @include flex();
       a {
         display: inline-block;
         color: #b0b0b0;
@@ -71,11 +75,7 @@ export default {
         text-align: center;
         color: #ffffff;
         .icon-cart {
-          display: inline-block;
-          width: 16px;
-          height: 12px;
-          background: url("/imgs/icon-cart-checked.png") no-repeat center;
-          background-size: contain;
+          @include bgImg(16px, 12px, "/imgs/icon-cart-checked.png");
           margin-right: 4px;
         }
       }
@@ -84,58 +84,70 @@ export default {
   .nav-header {
     .container {
       height: 112px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      .header-logo{
+      @include flex();
+      .header-logo {
         display: inline-block;
         width: 55px;
         height: 55px;
-        background-color: #FF6600;
-        a{
+        background-color: #ff6600;
+        a {
           display: inline-block;
           width: 110px;
           height: 155px;
-          &:before{
-            content:' ';
-            display: inline-block;
-            width: 55px;
-            height: 55px;
-            background: url('/imgs/mi-logo.png') no-repeat center;
-            background-size: 55px;
-            transition: margin .2s;
+          &:before {
+            content: " ";
+            @include bgImg(55px, 55px, "/imgs/mi-logo.png", 55px);
+            transition: margin 0.2s;
           }
-          &:after{
-            content:'';
-            display: inline-block;
-            width: 55px;
-            height: 55px;
-            background: url('/imgs/mi-home.png') no-repeat center;
-            background-size: 55px;
+          &:after {
+            content: "";
+            @include bgImg(55px, 55px, "/imgs/mi-home.png", 55px);
           }
-          &:hover:before{
+          &:hover:before {
             margin-left: -55px;
-            transition: margin .2s;
+            transition: margin 0.2s;
           }
         }
       }
-      .header-menu{
+      .header-menu {
         display: inline-block;
-        width: 200px;
+        width: 643px;
         padding-left: 209px;
-        .item-menu{
+        .item-menu {
           display: inline-block;
           color: #333333;
           font-weight: bold;
           font-size: 16px;
           line-height: 112px;
-          span{
+          margin-right: 20px;
+          span {
             cursor: pointer;
           }
-          &:hover{
-            
+          &:hover {
           }
-        }        
+        }
+      }
+      .header-search {
+        width: 319px;
+        .wrapper {
+          height: 50px;
+          border: 1px solid #e0e0e0;
+          display: flex;
+          align-items: center;
+          input {
+            border: none;
+            box-sizing: border-box;
+            border-right: 1px solid #e0e0e0;
+            width: 264px;
+            height: 50px;
+            padding-left: 14px;
+          }
+          a {
+            display: inline-block;
+            @include bgImg(18px, 18px, "/imgs/icon-search.png");
+            margin-left: 17px;
+          }
+        }
       }
     }
   }
