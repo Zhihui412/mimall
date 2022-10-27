5b8cb143a4560e415e8f2143bdec2e4cefdb946d
huangzhihui <leononon412@gmail.com>
2022-10-27 20:51:48 +0800

    add 首页轮播图菜单实现2

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,7 +7,16 @@
             <ul class="menu-wrap">
               <li class="menu-item">
                 <a href="javascript:;">手机 电话卡</a>
-                <div class="childern"></div>
+                <div class="children">
+                  <ul v-for="(item,i) in menuList" :key="i">
+                    <li v-for="(sub,j) in item" :key="j">
+                      <a :href="sub?'/#/product/' + sub.id:''">
+                        <img :src="sub ? sub.img : '/imgs/item-box-1.png'" alt="">
+                        {{sub ? sub.name : '小米9'}}
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </li>
               <li class="menu-item">
                 <a href="javascript:;">电视盒子 电话卡</a>
@@ -106,6 +115,31 @@ export default {
           img: "/imgs/slider/slide-5.jpg",
         },
       ],
+      menuList:[
+        [
+          {
+            id:30,
+            img:'/imgs/item-box-1.png',
+            name:'小米CC9',
+          },
+          {
+            id:31,
+            img:'/imgs/item-box-2.png',
+            name:'小米8青春版',
+          },
+          {
+            id:32,
+            img:'/imgs/item-box-3.jpg',
+            name:'Redmi K20 Pro',
+          },
+          {
+            id:33,
+            img:'/imgs/item-box-4.jpg',
+            name:'移动5G专区',
+          }
+        ],
+        [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+      ],
     };
   },
 };
@@ -145,6 +179,40 @@ export default {
           }
           &:hover{
             background-color: $colorA;
+            .children{
+              display: block;
+            }
+          }
+          .children{
+            display: none;
+            width: 962px;
+            height: 451px;
+            background-color: $colorG;
+            position: absolute;
+            top: 0;
+            left: 264px;
+            border: 1px solid $colorH;
+            ul{
+              display: flex;
+              justify-content: space-between;
+              height: 75px;
+              li{
+                height: 75px;
+                line-height: 75px;
+                flex: 1;
+                padding-left: 23px;
+              }
+              a{
+                color:$colorB;
+                font-size: 14px;
+              }
+              img{
+                width: 42px;
+                height: 35px;
+                vertical-align: middle;
+                margin-right: 15px;
+              }
+            }
           }
         }
       }
