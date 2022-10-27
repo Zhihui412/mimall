dbe3c493c09f0a94a54dc6ca25973492e2a5de01
huangzhihui <leononon412@gmail.com>
2022-10-27 22:29:29 +0800

    add 手机商品实现2

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -80,16 +80,16 @@
               </a>
             </div>
             <div class="list-box">
-              <div class="list" v-for="(arr, index) in phoneList" :key="index">
-                <div class="item" v-for="(item, index) in arr" :key="index">
-                  <span>新品</span>
-                  <div class="img-img">
-                    <img src="" alt="" />
+              <div class="list" v-for="(arr, i) in phoneList" :key="i">
+                <div class="item" v-for="(item, j) in arr" :key="j">
+                  <span :class="{ 'new-pro': j % 2 == 0 }">新品</span>
+                  <div class="item-img">
+                    <img :src="item.mainImage" alt="" />
                   </div>
                   <div class="item-info">
-                    <h3>小米9</h3>
-                    <p>骁龙855，索尼4800万超广角微距</p>
-                    <p class="price">2999元</p>
+                    <h3>{{ item.name }}</h3>
+                    <p>{{ item.subtitle }}</p>
+                    <p class="price">{{ item.price }}元</p>
                   </div>
                 </div>
               </div>
@@ -203,11 +203,26 @@ export default {
           img: "/imgs/ads/ads-4.jpg",
         },
       ],
-      phoneList: [
-        [1, 1, 1, 1],
-        [1, 1, 1, 1],
-      ],
+      phoneList: [],
     };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.axios
+        .get("/products", {
+          params: {
+            categoryId: 100012,
+            pageSize: 14,
+          }
+        })
+        .then((res) => {
+          res.list = res.list.slice(6,14)
+          this.phoneList = [res.list.slice(0, 4), res.list.slice(4, 8)];
+        });
+    },
   },
 };
 </script>
@@ -340,9 +355,22 @@ export default {
             background-color: $colorG;
             text-align: center;
             span {
+              display: inline-block;
+              width: 67px;
+              height: 24px;
+              font-size: 14px;
+              line-height: 24px;
+              color: $colorG;
+              &.new-pro {
+                background-color: #7ecf68;
+              }
+              &.kill-pro {
+                background-color: #e82626;
+              }
             }
             .item-img {
               img {
+                width: 100%;
                 height: 195px;
               }
             }
