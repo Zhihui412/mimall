1c4426ae7c7246921b4d62ebcab439487a093484
huangzhihui <leononon412@gmail.com>
2022-11-02 17:46:00 +0800

    add 第十章

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -129,7 +129,7 @@ export default {
     return {
       phoneList: [],
     };
-  },
+  }, 
   computed:{
       // username(){
       //   return this.$store.state.username
@@ -210,30 +210,6 @@ export default {
       position: relative;
       height: 112px;
       @include flex();
-      .header-logo {
-        display: inline-block;
-        width: 55px;
-        height: 55px;
-        background-color: #ff6600;
-        a {
-          display: inline-block;
-          width: 110px;
-          height: 155px;
-          &:before {
-            content: " ";
-            @include bgImg(55px, 55px, "/imgs/mi-logo.png", 55px);
-            transition: margin 0.2s;
-          }
-          &:after {
-            content: "";
-            @include bgImg(55px, 55px, "/imgs/mi-home.png", 55px);
-          }
-          &:hover:before {
-            margin-left: -55px;
-            transition: margin 0.2s;
-          }
-        }
-      }
       .header-menu {
         display: inline-block;
         width: 643px;
