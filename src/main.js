d745248e407bc335ec1b2463d16261e4e53b4478
huangzhihui <leononon412@gmail.com>
2022-11-09 12:57:00 +0800

    ADD 13

--- src/main.js
+++ src/main.js
@@ -37,9 +37,14 @@ axios.interceptors.response.use(function(response) {
     }    
   } else{
     Message.warning(res.msg)
-    return Promise.reject();
+    return Promise.reject(res);
   }
-});
+},(error) =>{
+  let res = error.response;
+  Message.error(res.data.message)
+  return Promise.reject(error);
+}
+);
 
 Vue.use(VueAxios,axios);
 Vue.use(VueCookie);
